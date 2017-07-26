9a0ff82e24e90f8615068de465b3c5d1e204d981
Sachin Pandey <sach@lanl.gov>
2017-07-26 11:35:50 -0600

    Redid batch_syntheticgw sims for simple species and reran 1d base case. Also started moving paper figures to a seperate folder.

--- chrome-dithionite-tests/batch_syntheticgw/eq_simpleSpecies_calcite/calcite.jl
+++ chrome-dithionite-tests/batch_syntheticgw/eq_simpleSpecies_calcite/calcite.jl
@@ -6,51 +6,46 @@ df = DataFrames
 
 myvar = [
 "4-pH obs (1) (0.5 0.5 0.5)"                            
-"6-Total H+ [M] obs (1) (0.5 0.5 0.5)"                  
-"7-Total O2(aq) [M] obs (1) (0.5 0.5 0.5)"              
-"8-Total CrO4-- [M] obs (1) (0.5 0.5 0.5)"              
-"9-Total S2O4-- [M] obs (1) (0.5 0.5 0.5)"              
-"10-Total S2O3-- [M] obs (1) (0.5 0.5 0.5)"             
-"11-Total SO3-- [M] obs (1) (0.5 0.5 0.5)"              
-"12-Total SO4-- [M] obs (1) (0.5 0.5 0.5)"              
-"13-Total Fe+++ [M] obs (1) (0.5 0.5 0.5)"              
-"14-Total Cr+++ [M] obs (1) (0.5 0.5 0.5)"              
-"15-Total HCO3- [M] obs (1) (0.5 0.5 0.5)"              
-"16-Total Ca++ [M] obs (1) (0.5 0.5 0.5)"               
-"17-Total K+ [M] obs (1) (0.5 0.5 0.5)"                 
+"5-Total H+ [M] obs (1) (0.5 0.5 0.5)"                  
+"6-Total O2(aq) [M] obs (1) (0.5 0.5 0.5)"              
+"7-Total CrO4-- [M] obs (1) (0.5 0.5 0.5)"              
+"8-Total S2O4-- [M] obs (1) (0.5 0.5 0.5)"              
+"9-Total S2O3-- [M] obs (1) (0.5 0.5 0.5)"              
+"10-Total SO3-- [M] obs (1) (0.5 0.5 0.5)"              
+"11-Total SO4-- [M] obs (1) (0.5 0.5 0.5)"              
+"12-Total Fe+++ [M] obs (1) (0.5 0.5 0.5)"              
+"13-Total Cr+++ [M] obs (1) (0.5 0.5 0.5)"              
+"14-Total HCO3- [M] obs (1) (0.5 0.5 0.5)"              
+"15-Total Ca++ [M] obs (1) (0.5 0.5 0.5)"               
+"16-Total K+ [M] obs (1) (0.5 0.5 0.5)"                 
+"17-Total Na+ [M] obs (1) (0.5 0.5 0.5)"                
 "18-Fe(OH)3(s) VF obs (1) (0.5 0.5 0.5)"                
 "19-Cr(OH)3(s) VF obs (1) (0.5 0.5 0.5)"                
-"20-Calcite VF obs (1) (0.5 0.5 0.5)"    
+"20-Calcite VF obs (1) (0.5 0.5 0.5)"                   
 ]
 
 # results = sachFun.readObsDataset("calcite-obs-0.tec",myvar,dataframe=true)
 results = sachFun.readObsDataset("calcite-obs-0.tec",myvar)
-results2 = sachFun.readObsDataset("initial_calcite_simpleSpecies-obs-0.tec",myvar)
+results2 = sachFun.readObsDataset("calcite_initial-obs-0.tec",myvar)
 results2df = df.DataFrame(name=myvar,conc=results[end,2:end],ic=results2[end,2:end],error = results[end,2:end]-results2[end,2:end])
-
+# 
 myvar = [
-"4-pH obs (1) (0.5 0.5 0.5)"                  
-"5-Total A(aq) [M] obs (1) (0.5 0.5 0.5)"     
-"6-Total H+ [M] obs (1) (0.5 0.5 0.5)"        
-"7-Total O2(aq) [M] obs (1) (0.5 0.5 0.5)"    
-"8-Total CrO4-- [M] obs (1) (0.5 0.5 0.5)"    
-"9-Total S2O4-- [M] obs (1) (0.5 0.5 0.5)"    
-"10-Total S2O3-- [M] obs (1) (0.5 0.5 0.5)"   
-"11-Total SO3-- [M] obs (1) (0.5 0.5 0.5)"    
-"12-Total SO4-- [M] obs (1) (0.5 0.5 0.5)"    
-"13-Total Fe+++ [M] obs (1) (0.5 0.5 0.5)"    
-"14-Total Cr+++ [M] obs (1) (0.5 0.5 0.5)"    
-"15-Total HCO3- [M] obs (1) (0.5 0.5 0.5)"    
-"16-Total Ca++ [M] obs (1) (0.5 0.5 0.5)"     
-"17-Total K+ [M] obs (1) (0.5 0.5 0.5)"  
+"4-pH obs (1) (0.5 0.5 0.5)"                            
+"5-Total H+ [M] obs (1) (0.5 0.5 0.5)"                  
+"6-Total O2(aq) [M] obs (1) (0.5 0.5 0.5)"              
+"7-Total CrO4-- [M] obs (1) (0.5 0.5 0.5)"              
+"8-Total S2O4-- [M] obs (1) (0.5 0.5 0.5)"              
+"9-Total S2O3-- [M] obs (1) (0.5 0.5 0.5)"              
+"10-Total SO3-- [M] obs (1) (0.5 0.5 0.5)"              
+"11-Total SO4-- [M] obs (1) (0.5 0.5 0.5)"              
+"12-Total Fe+++ [M] obs (1) (0.5 0.5 0.5)"              
+"13-Total Cr+++ [M] obs (1) (0.5 0.5 0.5)"              
+"14-Total HCO3- [M] obs (1) (0.5 0.5 0.5)"              
+"15-Total Ca++ [M] obs (1) (0.5 0.5 0.5)"               
+"16-Total K+ [M] obs (1) (0.5 0.5 0.5)"                 
+"17-Total Na+ [M] obs (1) (0.5 0.5 0.5)"                
 ]
 
-results3 = sachFun.readObsDataset("injectant_calcite_simpleSpecies-obs-0.tec",myvar)
-results3df = df.DataFrame(name=myvar,conc=results3[end,2:end])
-
-results4 = sachFun.readObsDataset("injectant_calcite_simpleSpecies_cleanWater-obs-0.tec",myvar)
-results4df = df.DataFrame(name=myvar,conc=results4[end,2:end])
-
-# results5 = sachFun.readObsDataset("injectant_calcite_simpleSpecies_cleanWater_check-obs-0.tec",myvar)
-# results5df = df.DataFrame(name=myvar,conc=results5[end,2:end])
-# results5df = df.DataFrame(name=myvar,conc=results4[end,2:end],ic=results5[end,2:end],error = results4[end,2:end]-results5[end,2:end])
+results3 = sachFun.readObsDataset("calcite_injectant-obs-0.tec",myvar)
+results4 = sachFun.readObsDataset("calcite_injectant_check-obs-0.tec",myvar)
+results4df = df.DataFrame(name=myvar,inj=results3[end,2:end],inj_check=results4[end,2:end],error = results3[end,2:end]-results4[end,2:end])
--- chrome-dithionite-tests/figures_paper/cvsx_step1.jl
+++ chrome-dithionite-tests/figures_paper/cvsx_step1.jl
@@ -4,47 +4,33 @@ using PyPlot
 plt = PyPlot
 
 filename = "1d-allReactions-10m-uniformVelocity"
-
-# myvar = [
-# "pH",
-# "Total_O2(aq) [M]",
-# "Total_CrO4-- [M]",
-# "Fe(OH)3(s)_VF",
-# "Cr(OH)3(s)_VF",
-# "Calcite_VF",
-# "slow_Fe++ [mol_m^3]",
-# "fast_Fe++ [mol_m^3]",
-# ]
+dirname = "/lclscratch/sach/Programs/pflotran-dithionite/chrome-dithionite-tests/1d_parameterTests"
 
 myvar = [
-"Total_O2(aq) [M]",
-"Total_CrO4-- [M]",
+"Total_S2O4-- [M]",
 "Fe(OH)3(s)_VF",
-"Cr(OH)3(s)_VF",
-"slow_Fe++ [mol_m^3]",
 "fast_Fe++ [mol_m^3]",
+"slow_Fe++ [mol_m^3]",
 ]
 
 coord_name = "X"
-mytime = [115,215,315]
+mytime = [5,10,15]
 
 # get the results first
 results = Dict()
 for i in 1:length(myvar)
     results[myvar[i]] = Dict()
   for j in 1:length(mytime)
-    results[myvar[i]][string(mytime[j])] = sachFun.readh5_1D("$(filename).h5",[myvar[i]],coord_name,mytime[j])
+    results[myvar[i]][string(mytime[j])] = sachFun.readh5_1D(joinpath(dirname,"$(filename).h5"),[myvar[i]],coord_name,mytime[j])
   end
 end
 
 # PLOT RESULTS
 coolnames = [
-L"\mathrm{O_2(aq)\, [M]}",
-L"\mathrm{CrO_4^{-2}\, [M]}",
+L"\mathrm{S_2O_4^{-2}\, [M]}",
 L"\mathrm{Fe(OH)_3(s)\, [wt\%]}",
-L"\mathrm{Cr(OH)_3(s)\, [wt\%]}",
-L"\mathrm{Fe(II)\, slow\, [mol\, g_{sediment}^-]}",
 L"\mathrm{Fe(II)\, fast\, [mol\, g_{sediment}^-]}",
+L"\mathrm{Fe(II)\, slow\, [mol\, g_{sediment}^-]}",
 ]
 
 timeunits = "day"
@@ -62,56 +48,46 @@ majorFormatter2 = matplotlib[:ticker][:FormatStrFormatter]("%0.1f")
 rho_bulk = 1200 # kg/m^3
 mv_feoh3 = 34.36 # cm^3/mole
 mw_feoh3 = 106.87 # g/mole
-mv_croh3 = 33.1 # cm^3/mole
-mw_croh3 = 103.0181 # g/mole
 
-fig, ax = plt.subplots(2,3,figsize=(6.5,3.75), sharex=true)
+fig, ax = plt.subplots(2,2,figsize=(4.33,3.75), sharex=true)
 for i in 1:length(myvar)
     for j in 1:length(mytime)
         if contains(myvar[i],"Fe(OH)3(s)_VF")
             ax[i][:plot](results[myvar[i]][string(mytime[j])][:,1],results[myvar[i]][string(mytime[j])][:,2]*100*(mw_feoh3/1000)/rho_bulk*100^3/mv_feoh3,label=mylabel[j],c=mycmap(j),lw=linewidth,ls = "-")
-        elseif contains(myvar[i],"Cr(OH)3(s)_VF")
-            ax[i][:plot](results[myvar[i]][string(mytime[j])][:,1],results[myvar[i]][string(mytime[j])][:,2]*100*(mw_croh3/1000)/rho_bulk*100^3/mv_croh3,label=mylabel[j],c=mycmap(j),lw=linewidth,ls = "-")
-
         elseif contains(myvar[i],"slow_Fe++") || contains(myvar[i],"fast_Fe++")
             ax[i][:plot](results[myvar[i]][string(mytime[j])][:,1],results[myvar[i]][string(mytime[j])][:,2]/(rho_bulk*1000),label=mylabel[j],c=mycmap(j),lw=linewidth,ls = "-")
         else
             ax[i][:plot](results[myvar[i]][string(mytime[j])][:,1],results[myvar[i]][string(mytime[j])][:,2],label=mylabel[j],c=mycmap(j),lw=linewidth,ls = "-")
-        end
+     end
     ax[i][:tick_params](labelsize=mysize)
     ax[i][:set_ylabel]("$(coolnames[i])",size=mysize)  
     ax[i][:set_xlim]([0,maximum(results[myvar[i]][string(mytime[j])][:,1])])
     ax[i][:set_xlim]([0,10])
     ax[i][:set_xlim]([0,4])
-    end
+  end
 end
 ax[1][:yaxis][:set_major_formatter](majorFormatter)
-ax[2][:yaxis][:set_major_formatter](majorFormatter)
-ax[3][:yaxis][:set_major_formatter](majorFormatter2)
-ax[4][:yaxis][:set_major_formatter](majorFormatter2)
-ax[5][:yaxis][:set_major_formatter](majorFormatter)
-ax[6][:yaxis][:set_major_formatter](majorFormatter)
+ax[2][:yaxis][:set_major_formatter](majorFormatter2)
+ax[3][:yaxis][:set_major_formatter](majorFormatter)
+ax[4][:yaxis][:set_major_formatter](majorFormatter)
+ax[4][:legend](loc=1,frameon=false,fontsize=mysize-2)
 
-ax[6][:legend](loc=1,frameon=false,fontsize=mysize-2)
-
-labeledxaxes = [2,4,6]
+labeledxaxes = [2,4]
 for i in 1:length(labeledxaxes)
     ax[labeledxaxes[i]][:set_xlabel](L"\mathrm{Distance\; from\; inlet\;[m]}",size=mysize)
     ax[labeledxaxes[i]][:set_xticks](0:4)
 end
 
-tight_layout(pad = 0.1, w_pad=0.0, h_pad = 1.0)
+tight_layout(pad = 0.1, w_pad=0.15, h_pad = 1.0)
 
-for i in 3:4
+# make box widths the same as other figure
+myboxw = 0.1974437237945494*6.5/(6.5/3*2)
+myboxh = 0.39284274193548396
+for i in 1:4
 box = ax[i][:get_position]()
-ax[i][:set_position]([box[:x0]-0.03, box[:y0], box[:width], box[:height]])
-end
-
-for i in 5:6
-box = ax[i][:get_position]()
-ax[i][:set_position]([box[:x0]-0.015, box[:y0], box[:width], box[:height]])
+ax[i][:set_position]([box[:x0], box[:y0], myboxw, myboxh])
 end
 
 fig[:canvas][:draw]() # Update the figure
-savefig("paper_cvsx_step2.png",dpi=100)
+savefig("cvsx_step1.png",dpi=100)
 close()
